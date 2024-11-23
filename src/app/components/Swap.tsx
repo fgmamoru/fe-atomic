43bd94dbe652ba313bef246dfd6c515ecc267c10
Fabian Gutierrez <fega.hg@gmail.com>
2024-11-23 00:50:05 -0500

    fix: connect wallet

--- src/app/components/Swap.tsx
+++ src/app/components/Swap.tsx
@@ -105,6 +105,7 @@ export function DexSwapTab() {
                 onClick={() => {
                     if (!mainModel.address) {
                         open();
+                        return;
                     }
 
                     setWaitingTransactionModalOpen(true);
