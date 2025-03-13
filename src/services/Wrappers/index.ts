6e4b0330d501b78b344073d696f0dc35684ef073
fsova <f@sova.network>
2025-03-13 18:47:33 -0500

    fix build

--- src/services/Wrappers/index.ts
+++ src/services/Wrappers/index.ts
@@ -1,6 +1,4 @@
 export * from './AtomicDex.wrapper';
-import { JettonDefaultWallet } from './tact_JettonDefaultWallet';
 import { SampleJettonWallet } from './tact_SampleJettonWallet';
 
-export const JettonDefaultWalletContract = JettonDefaultWallet;
 export const SampleJettonWalletContract = SampleJettonWallet; 
