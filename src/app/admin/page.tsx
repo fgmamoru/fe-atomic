980fe0d6ee418b5cda97399f9f67eb6c6e488037
fsova <f@sova.network>
2025-03-19 07:34:31 -0500

    feat: Added atomic dex address

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,11 @@
 import { PageLayout } from "@/components/Layout/PageLayout";
+import { ATOMIC_DEX_CONTRACT_ADDRESS } from "@/services/config.service";
 
 export default function AdminPage() {
     return (
         <PageLayout>
             <h1>Admin</h1>
+            <p><b>DEX:</b> {ATOMIC_DEX_CONTRACT_ADDRESS}</p>
         </PageLayout>
     );
 }
